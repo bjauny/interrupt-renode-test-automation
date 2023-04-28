6b313f494719687eb0ccee72d79b0f623f12c86d
Bastien JAUNY <bastien.jauny@gmail.com>
2023-04-28 16:05:13 +0200

    Switched to DOCKER_WORKSPACE

--- docker-test.sh
+++ docker-test.sh
@@ -14,7 +14,7 @@ declare -r HOST_LOG_FILENAME=${HOST_LOG_PATH}/logs.txt
 
 declare -r DOCKER_TAG=renode_stm32f4
 declare -r DOCKER_WORKSPACE=/workspace
-declare -r DOCKER_TEST_RESULTS_PATH=/tmp/test_results
+declare -r DOCKER_TEST_RESULTS_PATH=${DOCKER_WORKSPACE}/test_results
 
 mkdir -p ${HOST_LOG_PATH}
 
@@ -31,7 +31,7 @@ if ! docker run \
   --env RENODE_CHECKOUT=/home/developer/renode \
   --workdir ${DOCKER_WORKSPACE} \
   ${DOCKER_TAG} \
-  sudo /bin/bash -c "./run_tests.sh 2>&1 > ${DOCKER_TEST_RESULTS_PATH}/logs.txt"
+  /bin/bash -c "./run_tests.sh 2>&1 > ${DOCKER_TEST_RESULTS_PATH}/logs.txt"
 then
   echo "FAILED"
   exit_code=1
