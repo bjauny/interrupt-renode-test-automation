75a87dc8317f5efd291a55ed7d471c69f8fd6e7e
Bastien JAUNY <bastien.jauny@gmail.com>
2023-05-05 11:54:26 +0200

    Finding the script

--- run_tests.sh
+++ run_tests.sh
@@ -2,5 +2,5 @@
 
 # Runs Robot Framework tests locally
 RENODE_CHECKOUT=${RENODE_CHECKOUT:-~/code/renode}
-ls -l ${PWD}
+ls -lh ~
 #${RENODE_CHECKOUT}/test.sh -t "${PWD}/tests/tests.yaml" --variable PWD_PATH:"${PWD}" -r "${PWD}/test_results"
