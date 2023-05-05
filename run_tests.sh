19ad8b283f9d900628a3a27bafffdc3eff031a32
Bastien JAUNY <bastien.jauny@gmail.com>
2023-05-05 11:43:00 +0200

    Debug test script

--- run_tests.sh
+++ run_tests.sh
@@ -1,10 +1,8 @@
 #!/bin/bash -e
 
 # Runs Robot Framework tests locally
-
-RENODE_CHECKOUT=${RENODE_CHECKOUT:-~/code/renode}
-
-ls -l ${PWD}
 echo ${RENODE_CHECKOUT}
-ls -l ~/code/renode
-${RENODE_CHECKOUT}/test.sh -t "${PWD}/tests/tests.yaml" --variable PWD_PATH:"${PWD}" -r "${PWD}/test_results"
+env
+RENODE_CHECKOUT=${RENODE_CHECKOUT:-~/code/renode}
+echo ${RENODE_CHECKOUT}
+#${RENODE_CHECKOUT}/test.sh -t "${PWD}/tests/tests.yaml" --variable PWD_PATH:"${PWD}" -r "${PWD}/test_results"
