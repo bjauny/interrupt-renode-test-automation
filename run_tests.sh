c02ccf84682e1730c160944564fb2fe484c42da4
Bastien JAUNY <bastien.jauny@gmail.com>
2023-05-05 11:30:15 +0200

    Toying with the tests

--- run_tests.sh
+++ run_tests.sh
@@ -4,4 +4,7 @@
 
 RENODE_CHECKOUT=${RENODE_CHECKOUT:-~/code/renode}
 
+echo ${PWD}
+echo ${RENODE_CHECKOUT}
+echo ~/code/renode
 ${RENODE_CHECKOUT}/test.sh -t "${PWD}/tests/tests.yaml" --variable PWD_PATH:"${PWD}" -r "${PWD}/test_results"
