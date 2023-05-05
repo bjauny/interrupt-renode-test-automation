486d0c6a06bd9fe2fbd91adf855b5f01ac6f698a
Bastien JAUNY <bastien.jauny@gmail.com>
2023-05-05 11:50:46 +0200

    Listing PWD

--- run_tests.sh
+++ run_tests.sh
@@ -2,5 +2,5 @@
 
 # Runs Robot Framework tests locally
 RENODE_CHECKOUT=${RENODE_CHECKOUT:-~/code/renode}
-ls -l ${RENODE_CHECKOUT}
+ls -l ${PWD}
 #${RENODE_CHECKOUT}/test.sh -t "${PWD}/tests/tests.yaml" --variable PWD_PATH:"${PWD}" -r "${PWD}/test_results"
