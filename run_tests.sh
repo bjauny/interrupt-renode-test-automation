3193bf338f219842294389258d4cb7bf119b0a71
Bastien JAUNY <bastien.jauny@gmail.com>
2023-05-05 11:49:01 +0200

    Listing repo

--- run_tests.sh
+++ run_tests.sh
@@ -1,8 +1,6 @@
 #!/bin/bash -e
 
 # Runs Robot Framework tests locally
-echo ${RENODE_CHECKOUT}
-env
 RENODE_CHECKOUT=${RENODE_CHECKOUT:-~/code/renode}
-echo ${RENODE_CHECKOUT}
+ls -l ${RENODE_CHECKOUT}
 #${RENODE_CHECKOUT}/test.sh -t "${PWD}/tests/tests.yaml" --variable PWD_PATH:"${PWD}" -r "${PWD}/test_results"
